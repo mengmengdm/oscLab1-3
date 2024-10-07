d154fe9ffb3653cc8e54ac430cb07ef7dd568dd6
haoran fang <haoran.fang@student.kuleuven.be>
2024-10-07 17:26:44 +0200

    lab1e1

---
+++ clab1gcc/lab1e1.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main(){
+int int1;
+printf("the size of int is %ld\n",sizeof(int1));
+return 0;
+}
---
+++ clab1gcc/lable1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main (){
+int int1;
+float float1;
+double double1;
+void void1;
+int *p;
+
+printf("the size of int is %d \n", sizeof(int1));
+return 0;
+}
